3221d8dc98647ec60f4db329af36511d9869ebfc
Mustafa Eren DEMİR <meren.demir@ayvos.com>
2024-02-15 20:17:47 +0300

    initial commit

---
+++ src/main/java/com/application/library/desktop/DesktopApplication.java
@@ -0,0 +1,13 @@
+package com.application.library.desktop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesktopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesktopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application/library/desktop/DesktopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.library.desktop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesktopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
